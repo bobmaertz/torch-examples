480b30fb9c9aae1c136fdc83344032a01af1196a
Bob Maertz <r.maertz@gmail.com>
2024-12-03 23:32:57 -0500

    Minor refactoring - prepping for cmd line opts

--- main.py
+++ main.py
@@ -26,13 +26,7 @@ class Net(nn.Module):
         return x
 
 
-
-if __name__=="__main__": 
-
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(device)
-
-
+def LoadData(): 
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -48,18 +42,14 @@ if __name__=="__main__":
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=2)
-    
-    classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
+    return trainloader, testloader
 
-    net = Net()
-
-    net.to(device)
-
+def Train(net, trainloader): 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
     
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(10):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -84,6 +74,22 @@ if __name__=="__main__":
                 running_loss = 0.0
     
     print('Finished Training')
+
+if __name__=="__main__": 
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
+
+
+    trainloader, testloader = LoadData()
+
+    classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    net = Net()
+
+    net.to(device)
+
+    Train(net, trainloader)
 
     # Save the model 
     PATH = './cifar_net.pth'
